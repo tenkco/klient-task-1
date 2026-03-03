ec759775491c821d9d1b0b76bdf66c9d431cb619
tenkco <sytnikkv@gmail.com>
2026-03-03 12:08:50 +0700

    practical 2

--- task1/js/main.js
+++ task1/js/main.js
@@ -3,6 +3,9 @@ let app = new Vue({
     data: {
         product: "Socks",
         description: "A pair of warm, fuzzy socks",
+        image: "./assets/vmSocks-green-onWhite.jpg",
+        altText: "A pair of socks",
+        linkText: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks"
     }
 })
 
