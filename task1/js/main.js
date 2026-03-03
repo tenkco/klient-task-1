9e096715858226ca70a7b1b6e0aa65bd0ac40bfa
tenkco <sytnikkv@gmail.com>
2026-03-03 20:19:39 +0700

    practical 8

--- task1/js/main.js
+++ task1/js/main.js
@@ -1,32 +1,90 @@
-let app = new Vue({
-    el: '#app',
-    data: {
-        product: "Socks",
-        brand: 'Vue Mastery',
-        description: "A pair of warm, fuzzy socks",
-        selectedVariant: 0,
-        altText: "A pair of socks",
-        linkText: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-        inventory: 0,
-        onSale: true,
-        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
-        variants: [
-            {
-                variantId: 2234,
-                variantColor: 'green',
-                variantImage: "./assets/vmSocks-green-onWhite.jpg",
-                variantQuantity: 10,
-            },
-            {
-                variantId: 2235,
-                variantColor: 'blue',
-                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
-                variantQuantity: 0,
-            }
-        ],
+Vue.component('product-details', {
+    template: `
+        <ul>
+            <li v-for="detail in details">{{ detail }}</li>
+        </ul>
+    `,
+    props: ["details"],
+});
 
-        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
-        cart: 0,
+Vue.component('product', {
+    template: `
+    <div class="product">
+        <div class="product-image">
+            <img :src="image" :alt="altText" />
+        </div>
+
+        <div class="product-info">
+            <h1>{{ title }}</h1>
+            <a :href="linkText">More products like this</a>
+            <p v-if="inStock > 10">In Stock</p>
+            <p v-else-if="inStock <= 10 && inStock > 0">Almost sold out!</p>
+            <p v-else :class="{outStock: !inStock}">Out of Stock</p>
+            <span v-if="onSale"> On Sale </span>
+            <p>{{ sale }}</p>
+            <product-details :details="details"></product-details>
+            <p>Shipping: {{ shipping }}</p>
+            <div
+                    class="color-box"
+                    v-for="(variant, index) in variants"
+                    :key="variant.variantId"
+                    :style="{ backgroundColor:variant.variantColor }"
+                    @mouseover="updateProduct(index)"
+            ></div>
+
+            <ul>
+                <li v-for="size in sizes">{{ size }}</li>
+            </ul>
+
+            <div class="cart">
+                <p>Cart({{ cart }})</p>
+            </div>
+            <button
+                    v-on:click="addToCart"
+                    :disabled="!inStock"
+                    :class="{ disabledButton: !inStock }">
+                Add to cart
+            </button> 
+            <br>
+            <button v-on:click="removeFromCart">Remove from cart</button>
+        </div>
+    </div>
+    `,
+    props: {
+        premium: {
+            type: Boolean,
+            required: true
+        }
+    },
+    data() {
+        return {
+            product: "Socks",
+            brand: 'Vue Mastery',
+            description: "A pair of warm, fuzzy socks",
+            selectedVariant: 0,
+            altText: "A pair of socks",
+            linkText: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+            inventory: 0,
+            onSale: true,
+            details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+            variants: [
+                {
+                    variantId: 2234,
+                    variantColor: 'green',
+                    variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                    variantQuantity: 10,
+                },
+                {
+                    variantId: 2235,
+                    variantColor: 'blue',
+                    variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                    variantQuantity: 0,
+                }
+            ],
+
+            sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+            cart: 0,
+        }
     },
     methods: {
         addToCart() {
@@ -36,7 +94,7 @@ let app = new Vue({
         removeFromCart() {
             this.cart -= 1
             if (this.cart <= 0) {
-            this.cart = 0
+                this.cart = 0
             }
         },
 
@@ -62,9 +120,23 @@ let app = new Vue({
             } else {
                 return this.brand + ' ' + this.product + ' are not on sale';
             }
-        }
+        },
+
+        shipping() {
+            if (this.premium) {
+                return "Free";
+            } else {
+                return 2.99
+            }
+        },
 
     },
+})
 
+
+let app = new Vue({
+    el: '#app',
+    data: {
+        premium: true
+    },
 });
-
